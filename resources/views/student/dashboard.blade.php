c3447453bc4da646cc9e0328afbe094f18c88179
maskibrk <maskiiski@gmail.com>
2026-07-24 17:34:29 +0100

    blade fixes

---
+++ resources/views/student/dashboard.blade.php
@@ -0,0 +1,18 @@
+<x-layouts::app :title="__('Student Dashboard')">
+
+    <div class="flex flex-col gap-6">
+
+        <div>
+            <h1 class="text-3xl font-bold">
+                Student Dashboard
+            </h1>
+
+            <p class="text-gray-500">
+                Welcome! View your academic information and activities.
+            </p>
+        </div>
+
+
+    </div>
+
+</x-layouts::app>
